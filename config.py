7ab57b726873e2184d488c0a306db13af09c4aa8
Youtuberec2arc <heistbeecrypto@gmail.com>
2026-05-03 20:09:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "8720338412:AAH-TJvQiLuKaf4-PYsStJAGt4SAjLY
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "Rec2arcBot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
